7ed6e30cd6f66647a287d7ff14f913933edb0001
rukiyearican <rukiyearican@gmail.com>
2023-07-19 16:59:07 +0300

    added login/logout

--- base/views.py
+++ base/views.py
@@ -4,9 +4,24 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Task
 
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+
+
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
